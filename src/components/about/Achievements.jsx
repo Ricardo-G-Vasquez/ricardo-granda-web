03c4e9c427ceb391bfc7c0eb2b9e59cc284ba103
ricardo <me@ricardogranda.com>
2023-02-21 17:58:46 +0100

    style(deleting unused letter): deleting unused letter in class
    deleting unused letter in class

--- src/components/about/Achievements.jsx
+++ src/components/about/Achievements.jsx
@@ -10,7 +10,7 @@ const Achievements = () => {
         <div className="col-6" key={i}>
           <div className="box-stats with-margin">
             <h3 className="poppins-font position-relative">{val.title}</h3>
-            <p className="open-sans-font m-0 position-relative text-uppercase i">
+            <p className="open-sans-font m-0 position-relative text-uppercase">
               {val.subTitle1} <span className="d-block">{val.subTitle2}</span>
             </p>
           </div>
