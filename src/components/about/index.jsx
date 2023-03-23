40ec1df60e08f95be01927e5e366a06701ed51a8
ricardo <me@ricardogranda.com>
2023-03-23 02:04:00 +0100

    feat(implementing custom link): implementing custom link
    implementing custom link for opening target blank link

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -4,7 +4,7 @@ import Education from "./Education";
 import Experience from "./Experience/Experience";
 import PersonalInfo from "./PersonalInfo";
 import Skills from "./Skills";
-import heroImgMobile from "../../../public/assets/img/hero/img-mobile.jpg";
+import heroImgMobile from "../../../public/assets/img/hero/ricardo_granda_dev.jpg";
 import Image from "next/image";
 
 const index = () => {
